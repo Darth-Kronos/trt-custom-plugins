0598f024049fd7a911b5deb811dc80e89c00079d
Darth-Kronos <purushothaman.syadav@gmail.com>
2023-10-07 03:09:16 +0000

    Model creation

---
+++ plugin/cosLUPlugin/models.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.models import resnet50, ResNet50_Weights
+
+class CosLU(nn.Module):
+    def __init__(self):
+        super(CosLU, self).__init__()
+        self.a = nn.Parameter(torch.empty(1))
+        self.b = nn.Parameter(torch.empty(1))
+
+        nn.init.constant_(self.a, 1.0)
+        nn.init.constant_(self.b, 1.0)
+
+    def forward(self, x):
+        return F.sigmoid(x) * (x + self.a * torch.cos(self.b * x))
+
+class CustomResNet(nn.Module):
+    def __init__(self, custom_activation):
+        super(CustomResNet, self).__init__()
+        
+        # Load a pre-trained ResNet model
+        self.resnet = resnet50(weights=ResNet50_Weights.DEFAULT)
+        self.custom_activation = custom_activation
+
+        # Replace activations with custom_activation recursively
+        self.replace_activation(self.resnet)
+
+    def replace_activation(self, module):
+        for name, child in module.named_children():
+            if isinstance(child, nn.ReLU):
+                setattr(module, name, self.custom_activation())
+            else:
+                self.replace_activation(child)
+
+    def forward(self, x):
+        return self.resnet(x)
